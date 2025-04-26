6dd8de7b751ed250c4d36859765d60fdabee3161
Eszter Kovacs <koveszter7@gmail.com>
2025-04-26 14:51:07 +0200

    add2

---
+++ app.py
@@ -0,0 +1,18 @@
+import dash
+from dash import dcc, html
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Helló Világ Dash app!"),
+    dcc.Graph(
+        figure={
+            "data": [{"x": [1, 2, 3], "y": [4, 1, 2], "type": "line"}],
+            "layout": {"title": "Egyszerű grafikon"}
+        }
+    )
+])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
